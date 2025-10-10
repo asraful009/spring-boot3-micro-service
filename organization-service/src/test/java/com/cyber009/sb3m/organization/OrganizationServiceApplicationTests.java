4da72734fda7a7c6b062223f7f846f72995b199b
Asraful Islam <asraful009@gmail.com>
2025-10-10 12:35:45 +0600

    org service add

---
+++ organization-service/src/test/java/com/cyber009/sb3m/organization/OrganizationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyber009.sb3m.organization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganizationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
