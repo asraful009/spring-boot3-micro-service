ad1f3ca79d65cd78236a5f790db0619d9d3d974d
Asraful Islam <asraful009@gmail.com>
2025-10-11 19:07:13 +0600

    common add

---
+++ common-lib/src/main/java/com/cyber009/sb3m/common/CommonLibApplication.java
@@ -0,0 +1,13 @@
+package com.cyber009.sb3m.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonLibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonLibApplication.class, args);
+	}
+
+}
---
+++ common-lib/src/test/java/com/cyber009/sb3m/common/CommonLibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyber009.sb3m.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonLibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
