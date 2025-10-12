582d5750f4bbf9049f89caf1967d5f9f65709de9
Asraful Islam <asraful009@gmail.com>
2025-10-12 22:18:41 +0600

    org back

---
+++ organization-back-service/src/test/java/com/cyber009/sb3m/organization_back_service/OrganizationBackServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyber009.sb3m.organization_back_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganizationBackServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
